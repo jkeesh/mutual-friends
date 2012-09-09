3f8cfb033c81a417303cd1107ac693e4985409ba
Jeremy Keeshin <jkeesh@gmail.com>
2012-09-08 20:56:03 -0700

    update

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($user) {
 	$friends = $facebook->api('/me/friends');
 
 	foreach($friends as $friend){
-		$mutual_friends = $facebook->api('/me/mutalfriends/' + $friend['id'])
+		$mutual_friends = $facebook->api('/me/mutalfriends/' . $friend['id'])
 		print_r($mutual_friends);
 	}
 
