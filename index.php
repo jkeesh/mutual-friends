a1bbd27c99d13d18176925dec2bbff3d76f0e379
Jeremy Keeshin <jkeesh@gmail.com>
2012-09-08 20:34:41 -0700

    add sdk

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*require 'secrets.php'; // facebook secrets
+require 'facebook-php-sdk/src/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => FACEBOOK_APP_ID,
+  'secret' => FACEBOOK_SECRET,
+));
+
+// Get User ID
+$user = $facebook->getUser();
+
+
+print_r($user);
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+</style>
+<title>Most Mutual Friends</title>
+</head>
+
+<body>
+
+
+<?php
+
+	echo "hello";
+//	echo FACEBOOK_APP_ID;
+
+?>
+
+</body>
+</html>
